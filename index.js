5a6419f4813bf4ff8b61091b6713889ccb84b425
jvldwin <Mathematiciannguyen@gmail.com>
2022-01-06 22:30:20 +0700

    Pushing to heroku

--- index.js
+++ index.js
@@ -4,25 +4,10 @@ const PORT = process.env.PORT || 4000;
 
 console.log(`worker pid=${process.pid}`);
 
-server.get('/recipes/:id', async (req, reply) => {
-  console.log(`worker request pid=${process.pid}`);
-  const id = Number(req.params.id);
-  if (id !== 42) {
-    reply.statusCode = 404;
-    return { error: 'not_found' };
-  }
-  return {
-    producer_pid: process.pid,
-    recipe: {
-      id, name: 'Chicken Tikka Masala',
-      steps: 'Throw it in a pot...',
-      ingredients: [
-        { id: 1, name: 'Chicken', quantity: '1 lb', },
-        { id: 2, name: 'Sauce', quantity: '2 cups', }
-      ]
-    }
-  };
-});
+server.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
 server.listen(PORT, HOST, () => {
   console.log(`Producer running at http://${HOST}:${PORT}`);
 });
