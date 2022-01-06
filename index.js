69a75e8fcc9c0e26481d87ffbb52e990c7c33f3b
jvldwin <Mathematiciannguyen@gmail.com>
2022-01-06 22:36:52 +0700

    Pushing to heroku

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const server = require('fastify')();
-const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 4000;
 
 console.log(`worker pid=${process.pid}`);
@@ -8,6 +7,6 @@ server.get('/', function (request, reply) {
   reply.send({ hello: 'world' })
 })
 
-server.listen(PORT, HOST, () => {
-  console.log(`Producer running at http://${HOST}:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Producer running at ${PORT}`);
 });
